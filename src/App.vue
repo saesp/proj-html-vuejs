367d9f68f5c168b10cd6d2d3b45ec18d57dd7482
esposito.00@outlook.it <esposito.00@outlook.it>
2022-12-21 12:29:58 +0100

    inizio

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
+import { store } from './store.js'
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    AppFooter
+  },
+
+  data() {
+    return {
+      store,
+    }
+  },
+}
+
+</script>
+
+<template>
+  <AppHeader />
+
+  <main>
+
+  </main>
+
+  <AppFooter />
+</template>
+
+<style lang="scss">
+// @use './style/general.scss'
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+// @use '../style/general.scss' as *;
+</style>
